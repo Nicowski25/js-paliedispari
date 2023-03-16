a10a95c83c034ff69c2cf3dc515c0fbac2e4a6ef
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-16 16:57:23 +0100

    es1 fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 //chiedere all'utente di inserire una parola e verificare se la parola inserita è palindroma
 
-const parolapalindroma = 'aerea'
+let parolapalindroma = prompt('inserisci una parola per controllare se è palindroma')
 
 //creiamo la funzione 
 function palindroma(parola) {
-    //creiamo un array delle lettere nella prima parola
+/*     //creiamo un array delle lettere nella prima parola
     let wordArray = []
     let wordArrayOpposite = []
     for (let i = 0; i < parola.length; i++) {
@@ -15,29 +15,31 @@ function palindroma(parola) {
         //stampiamo l'array
         console.log(wordArray);
     }
-    console.log(wordArray);
+    console.log(wordArray); */
 
     //secondo ciclo che inverte la parola
+    let parolaReverse = ""
     for (let i = parola.length-1; i >= 0; i--) { //ciclo che inverte il conteggio dell'indice
         //variabile lettera = valore di ogni lettera della parola ma letta al contrario
         let lettera = parola[i];
         //aggiungiamo la lettera all'arrayOpposto
-        wordArrayOpposite.push(lettera)
+        parolaReverse += lettera
+        //parolaReverse.push(lettera)
         //stampiamo l'array
-        console.log(wordArrayOpposite);
+        console.log(parolaReverse);
     }
-    console.log(wordArrayOpposite);
+    console.log(parolaReverse);
 
-    //controlliamo che gli elementi[i] degli array siano entrambi uguali
-    for (let i = 0; i < wordArray.length; i++) {
-        if (!(wordArray[i] == wordArrayOpposite[i])) {
-            console.log('la parola non è palindroma');
-            return false;
-        } else {
-            console.log('la parola è palindroma');
-        }
+    //controlliamo che parola e parola reverse siano uguali
     
+    if (parola == parolaReverse) {
+        console.log('la parola è palindroma');
+    } else {
+        console.log('la parola non è palindroma');
     }
+
 }
 
 palindroma(parolapalindroma)
+
+//
